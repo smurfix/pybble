95ab03382e468feb3fca3810e99a6c30a9b953d6
Matthias Urlichs <smurf@smurf.noris.de>
2008-11-18 22:53:24 +0100

    Forgot to add pybble/database.py

---
+++ pybble/database.py
@@ -0,0 +1,51 @@
+import os
+import settings
+
+from types import ModuleType, FunctionType
+from werkzeug import cached_property
+from sqlalchemy import MetaData, create_engine, Table, String, Boolean,\
+    Integer, Column, Text, Float, ForeignKey, DateTime
+from sqlalchemy.types import TypeDecorator, MutableType
+from sqlalchemy.orm import scoped_session, create_session, relation, Query, \
+    MapperExtension
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+
+if settings.DATABASE_TYPE == "sqlite":
+	dsn = 'sqlite:///'+settings.DATABASE_FILE
+elif settings.DATABASE_TYPE == "mysql":
+	dsn = 'mysql://%s:%s@%s/%s?charset=utf8' % (
+    settings.DATABASE_USER, settings.DATABASE_PASSWORD,
+    settings.DATABASE_HOST, settings.DATABASE_NAME)
+else:
+	raise ValueError("unsupported ddatabase type: use 'sqlite' or 'mysql'")
+
+engine = create_engine(dsn, pool_recycle=300, convert_unicode=True, echo=settings.DATABASE_DEBUG)
+metadata = MetaData(bind=engine)
+
+class ModelBase(object):
+    """Internal baseclass for `Model`."""
+
+session = scoped_session(lambda: create_session(engine, autoflush=True))
+
+Model = declarative_base(metadata=metadata,
+                         mapper=session.mapper)
+Model.query = session.query_property()
+
+def _make_module():
+    import sqlalchemy
+    from sqlalchemy import orm
+
+    db = ModuleType('db')
+    for mod in sqlalchemy, orm:
+        for key in mod.__all__:
+            setattr(db, key, getattr(mod, key))
+    #db.File = File
+    db.engine = engine
+    db.session = session
+    db.Model = Model
+    db.Query = Query
+    return db
+
+db = _make_module()
+del _make_module
+
