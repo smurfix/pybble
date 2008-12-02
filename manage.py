9320531af835bbfcd31d3d3e7fcea7bc12366b76
Matthias Urlichs <smurf@smurf.noris.de>
2008-12-02 15:56:12 +0100

    Use the Werkzeug debugger

--- manage.py
+++ manage.py
@@ -3,17 +3,26 @@
 
 from werkzeug import script
 import settings
+from werkzeug.debug import DebuggedApplication
+
+def res(x):
+	def r():
+		return x
+	return r
 
 def make_app():
-    from pybble.application import Pybble
-    return Pybble('sqlite:///'+settings.DATABASE_FILE)
+	from pybble.application import Pybble
+	app = Pybble('sqlite:///'+settings.DATABASE_FILE)
+	return app
 
 def make_shell():
-    from pybble import models, utils
-    application = make_app()
-    return locals()
+	from pybble import models, utils
+	application = make_app()
+	return locals()
 
-action_runserver = script.make_runserver(make_app, use_reloader=True)
+action_runserver = script.make_runserver(
+	res(DebuggedApplication(make_app(), evalex=True, show_hidden_frames=True)),
+	use_reloader=True)
 action_shell = script.make_shell(make_shell)
 action_initdb = make_app().init_database()
 action_initsite = make_app().init_site()
