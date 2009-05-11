4767ef360fd0a36a19134db91466d328f8f5e11f
Matthias Urlichs <smurf@smurf.noris.de>
2009-05-11 20:45:49 +0200

    NULLs -/

--- pybble/addon/jverein/__init__.py
+++ pybble/addon/jverein/__init__.py
@@ -234,10 +234,10 @@ accountnr: %s
 				self.betrag = betrag
 				self.zweck = zweck
 
-		r = db.session.execute("select id,datum,betrag,concat(zweck,' ',zweck2,' ',zweck3) from %s where konto_id=%d order by id desc" % (self.accountdb,self.accountnr))
+		r = db.session.execute("select id,datum,betrag,concat(zweck,' ',ifnull(zweck2,''),' ',ifnull(zweck3,'')) from %s where konto_id=%d order by id desc" % (self.accountdb,self.accountnr))
 		rr = r.fetchone()
 		while rr:
-			yield buchung(rr[0],rr[1],rr[2],rr[3])
+			yield buchung(rr[0],rr[1],rr[2],strip(rr[3]))
 			rr = r.fetchone()
 
 
