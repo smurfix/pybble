15b35f9a11764931a0a6f1f02c1ac4809172af74
Matthias Urlichs <matthias@urlichs.de>
2014-05-02 19:06:28 +0200

    Configure test to commit afterwards, for easier debugging

---
+++ test/conftest.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, division, unicode_literals
+##
+## This is part of Pybble, a WMS (Whatever Management System) based on
+## Jinja2/Haml, Werkzeug, Flask, and Optimism.
+##
+## Pybble is Copyright © 2009-2014 by Matthias Urlichs <matthias@urlichs.de>,
+## it is licensed under the GPLv3. See the file `README.md` for details
+## as well as hopeful statements by the author.
+##
+## This paragraph is auto-generated and may self-destruct at any time.
+## Please do not remove the next line, or insert any blank lines before it.
+##BP
+
+# content of conftest.py
+import pybble
+import pytest
+from pybble.core.db import db
+
+@pytest.fixture()
+def transact(self, request):
+	print("START",request)
+	def end():
+		print("END",request)
+		db.commit()
+	request.addfinalizer(end)
