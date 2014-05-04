471b0fd25fe5f0358d9b9a4e21b48d78c7bf50ce
Matthias Urlichs <matthias@urlichs.de>
2014-05-04 19:08:56 +0200

    Implement proper configuration reloading after changing stuff

---
+++ pybble/core/signal.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, division, unicode_literals
+##
+## This is part of Pybble, a WMS (Whatever Management System) based on
+## Jinja2/Haml, Werkzeug, Flask, and Optimism.
+##
+## Pybble is Copyright © 2009-2014 by Matthias Urlichs <matthias@urlichs.de>,
+## it is licensed under the GPLv3. See the file `README.md` for details
+## as well as hopeful statements by the author.
+##
+## This paragraph is auto-generated and may self-destruct at any time.
+## Please do not remove the next line, or insert any blank lines before it.
+##BP
+
+from blinker import signal
+
+all_apps = signal("all-apps", doc="""\
+					This signal will be triggered whenever something causes
+					all apps' state to be invalidated.""")
+app_list = signal("app-list", doc="""\
+					This signal will be triggered whenever something causes
+					the list of applications to change (e.g. create a new app).""")
+
