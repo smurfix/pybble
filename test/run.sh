39c64d1d90a2330e3dd12f346c85b889c0938369
Matthias Urlichs <matthias@urlichs.de>
2014-05-10 04:38:53 +0200

    Use getopt in test/run.sh, add options to keep the test DB

--- test/run.sh
+++ test/run.sh
@@ -10,53 +10,116 @@ export PYBBLE_SQL_DRIVER=sqlite
 export PYBBLE_SQL_DATABASE=/tmp/empty
 export PYBBLE_MEDIA_PATH=/tmp/empty
 
+usage() {
+cat >&2 <<END
+Usage: $(basename $0)  -- run Pybble in a test environment
+	-h	this help
+	-d	debug when a test fails / run using the debugger
+	-k	keep intermediate files
+	-n	don't rebuild the test database
+	-p	don't mangle assertions
+	-r	always rebuild the test database
+	XX	run script XX instead of testing
+END
+exit $1
+}
+
+KEEP=
+NOCHECK=
+DEBUG=
+PLAIN=
+REBUILD=
+export POSIXLY_CORRECT=1
+T="$(/usr/bin/getopt "+dhknpr" "$@")" || usage 1
+eval set -- "$T"
+for i
+do
+        case "$i"
+        in
+                -d)
+                        shift; DEBUG=y ;;
+                -h)
+                        usage 0 ;;
+                -n)
+                        shift; NOCHECK=y ;;
+                -k)
+                        shift; KEEP=y ;;
+                -p)
+                        shift; PLAIN=y ;;
+                -r)
+                        shift; REBUILD=y ;;
+                --)
+                        shift; break;;
+        esac
+done
+[ "$KEEP$REBUILD" = "yy" ] && usage 1
+
 D=/tmp/pybble/$USER
 mkdir -p $D
 
 skip=N
 
-rev=$(git rev-parse --short=9 HEAD)
-REV=$D/tag
-if [ -f $REV ] ; then
-	if [ $rev = $(cat $REV) -a -f $D/$rev.db ] ; then
-		skip=Y
-	else
-		rm -rf $D/$(cat $REV).*
-	fi
+NREV=$(git rev-parse --short=9 HEAD)
+rev="$D/tag"
+OREV=""
+redo=
+if [ -f $rev ] ; then
+	OREV="$(cat $rev)"
 fi
-if [ $skip = N ] ; then
-	echo $rev > $REV
-	PYBBLE_MEDIA_PATH=$D/$rev.files
-	PYBBLE_SQL_DATABASE=$D/$rev.db
+if [ -n "$REBUILD" ] ; then
+	redo=Y
+elif [ -n "$KEEP" ] ; then
+	if [ -z "$OREV" ] ; then
+		echo "No existing test database" >&2
+		exit 1
+	fi
+	redo=N
+elif [ "$OREV" = "$NREV" ] ; then
+	redo=N
+else
+	redo=Y
+fi
+
+if [ $redo = Y ] ; then
+	if [ -n "$OREV" ] ; then
+		rm -rf $D/"$OREV".*
+	fi
+	rm -f "$rev"
+	echo "$NREV" > "$rev"
+	PYBBLE_MEDIA_PATH="$D/$NREV"
+	PYBBLE_SQL_DATABASE="$D/$NREV"
 
 	./manage.py -t -S schema -x
 	./manage.py -t -S populate
+	echo "$NREV" > $rev
 else
-	PYBBLE_MEDIA_PATH=$D/$rev.files
-	PYBBLE_SQL_DATABASE=$D/$rev.db
+	PYBBLE_MEDIA_PATH="$D/$OREV".files
+	PYBBLE_SQL_DATABASE="$D/$OREV".db
 fi
 
 DATA=$(tempfile)
 SQL=$DATA.db
 rm $DATA
-cp -a $PYBBLE_SQL_DATABASE $SQL
-cp -a $PYBBLE_MEDIA_PATH $DATA
-PYBBLE_SQL_DATABASE=$SQL
-PYBBLE_MEDIA_PATH=$DATA
+cp -a "$PYBBLE_SQL_DATABASE" $SQL
+cp -a "$PYBBLE_MEDIA_PATH" $DATA
+PYBBLE_SQL_DATABASE="$SQL"
+PYBBLE_MEDIA_PATH="$DATA"
 
 SHELL=
 PY=python
-if [ "$1" = "-k" ] ; then
+ASS=
+if [ -n "$KEEP" ] ; then
 	shift
 	trap 'echo rm -r $DATA $SQL' 0 1 2 15
 else
 	trap 'rm -r $DATA $SQL' 0 1 2 15
-    if [ "$1" = "-p" ] ; then
+    if [ -n "$PLAIN" ] ; then
 		shift
-		ASS=--assert=plain
-    elif [ "$1" = "-d" ] ; then
+		ASS="$ASS --assert=plain"
+	fi
+    if [ -n "$DEBUG" ] ; then
 		PY=pdb
-		ASS="-s --pdb"
+		ASS="$ASS -s --pdb"
 		shift
 	fi
 fi
